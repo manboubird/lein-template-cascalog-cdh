420be1752c79e45b5e750b1aad3c57bffa3b35c7
manboubird <manboubird@gmail.com>
2013-08-05 23:58:41 +0900

    bump up mr1-cdh4.3.0

--- src/leiningen/new/cascalog_cdh/project.clj
+++ src/leiningen/new/cascalog_cdh/project.clj
@@ -4,10 +4,10 @@
   :jvm-opts ["-Xmx768m" "-server"]
   :aot :all
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [cascalog "1.10.1"]]
+                 [cascalog "1.10.2-mr1-cdh4.3.0"]]
   :profiles {:dev {:dependencies [[lein-midje "3.0.1"]
-                                  [cascalog/midje-cascalog "1.10.1"]]}
-             :provided {:dependencies [[org.apache.hadoop/hadoop-core "0.20.2-cdh3u2"]]}}
-  :repositories [["conjars.org" "http://conjars.org/repo"]
-                 ["cloudera" "https://repository.cloudera.com/content/groups/public/"]]
+                                  [cascalog/midje-cascalog "1.10.2-mr1-cdh4.3.0"]]}
+             :provided {:dependencies [[org.apache.hadoop/hadoop-core "2.0.0-mr1-cdh4.3.0"]]}}
+  :repositories {"conjars.org" "http://conjars.org/repo"
+                 "cloudera-releases" "https://repository.cloudera.com/artifactory/cloudera-repos"}
   )
